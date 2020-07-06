1771b368579049f4757eae895988e06ed5492e1c
王俊杰 <1648177163@qq.com>
2020-07-06 22:03:36 +0800

    增添account信息

---
+++ src/main/java/com/junjie/dao/IAccountDAO.java
@@ -0,0 +1,26 @@
+package com.junjie.dao;
+
+
+import com.junjie.domain.Account;
+import com.junjie.domain.AccountUser;
+
+import java.util.List;
+
+/**
+ * @author 黑马程序员
+ * @Company http://www.ithiema.com
+ */
+public interface IAccountDAO {
+
+    /**
+     * 查询所有账户，同时还要获取到当前账户的所属用户信息
+     * @return
+     */
+    List<Account> findAll();
+
+    /**
+     * 查询所有账户，并且带有用户名称和地址信息
+     * @return
+     */
+    List<AccountUser> findAllAccount();
+}
